6936b0d74626cc3b9fadb4a4e09850b64f68f7c5
Ifeoluwa Oduwaiye <71222572+Nalito@users.noreply.github.com>
2023-08-10 00:58:00 +0200

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a full-stack data scientist with over a year of working experience. Currently studying computer engineering at the Bells University of Technology.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Data Science",
+  "Machine Learning",
+  "Data Analysis",
+  "Web development",
+  "Deep Learning",
+  "Business Intelligence Enthusiast",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Join me on my quest to exploit data for good!!";
 
 const About = () => {
   return (
